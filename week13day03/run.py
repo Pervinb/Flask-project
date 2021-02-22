38823595630f91d550399e4bc3095c1e2be2eb62
parvin <bagirovp@gmail.com>
2021-02-22 04:59:04 +0400

    Image upload weak 13/day04

--- week13day03/run.py
+++ week13day03/run.py
@@ -2,9 +2,12 @@ from flask import Flask,redirect,request,render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate,MigrateCommand
 from flask_script import Manager
+import random
+import os
 app=Flask(__name__)
 # database and migration object creation
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database/data.db"
+app.config['UPLOAD_PATH']='static/upload'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 manager = Manager(app)
@@ -15,6 +18,7 @@ class Product(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     title=db.Column(db.String)
     content=db.Column(db.String)
+    photo=db.Column(db.String)
 
 
 
@@ -39,7 +43,12 @@ def adminIndex():
 @app.route('/admin/add',methods=['GET','POST'])
 def adminAddProduct():
     if request.method=='POST':
-        product=Product(title=request.form['productHeading'],content=request.form['productContent'])
+        randNumber=random.randint(3, 900);
+        f = request.files['productPhoto']
+        newName=f"profile{randNumber}.{f.filename.split('.')[1]}"
+        f.save(os.path.join(app.config['UPLOAD_PATH'],newName))
+        filePath=f"{app.config['UPLOAD_PATH']}/{f.filename}"
+        product=Product(title=request.form['productHeading'],content=request.form['productContent'],photo=filePath)
         db.session.add(product)
         db.session.commit()
         return redirect('/admin/all')
